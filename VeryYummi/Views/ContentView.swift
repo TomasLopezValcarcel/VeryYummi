ff4c167e596b694842380c865be019ac40936d69
Tomas Lopez-Valcarcel <lopez-valcarcel.t@etoncollege.org.uk>
2024-01-25 13:10:13 +0000

    start to add fields

--- VeryYummi/Views/ContentView.swift
+++ VeryYummi/Views/ContentView.swift
@@ -12,11 +12,33 @@ struct ContentView: View {
     @State private var kitchen = Kitchen()
     @State private var selectedFood: Int = 0
     
+    @State private var foodName = ""
+    @State private var foodQty = 0
+    @State private var foodUnit = ""
+    @State private var foodCategory = ""
+    @State private var foodExp = Date()
+    
+        
+
+
     var body: some View{
         Form{
-            VStack(alignment: .center, content: {
-                Text("\(kitchen.ingredients[selectedFood].displayIngredientsInfo())")
-            })
+            Section{
+                VStack(alignment: .center, content: {
+                    Text("\(kitchen.ingredients[selectedFood].displayIngredientsInfo())")
+                })
+            }
+            Section{
+                VStack(alignment: .center, content: {
+                    Text("Add a new ingredient").bold()
+                    TextField("Food name", text: $foodName)
+                    TextField("Quantity", value: $foodQty, formatter: NumberFormatter())
+                    TextField("Unit", text: $foodUnit)
+                    TextField("Category", text: $foodCategory)
+//                    Picker("Category", selection: $foodCategory)
+                    DatePicker("Expiry date", selection: $foodExp, displayedComponents: .date)
+                })
+            }
         }
     }
 }
